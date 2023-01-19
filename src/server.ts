03471bfe7c7803ff2e0a004ed74ac8b2853f1993
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-01-19 04:57:08 -0300

    feat: create new routes returning the completedHabits, and possibleHabits of the day

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,12 @@
-import { PrismaClient } from "@prisma/client"
 import cors from '@fastify/cors'
 import Fastify from "fastify"
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-const prismaClient = new PrismaClient()
 
 
 app.register(cors)
-app.get("/", async () => {
-  const habits = await prismaClient.habit.findMany({
-    where: {
-      title: {
-        startsWith: ""
-      }
-    }
-  })
-
-
-
-  return `hello nlw, ${JSON.stringify(habits)}`
-})
-
+app.register(appRoutes)
 app.listen({
   port: 3333
 }).then(() => {
